54095e125c24dca53343b6bdc4f6946afa45f7c8
johanaller <93586332+johanaller@users.noreply.github.com>
2023-11-25 14:01:25 +0100

    Initialize feature/solution-one-extension

---
+++ src/views/MainPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import MovieList from "../components/MovieList";
+import SearchBar from "../components/SearchBar";
+import { PageContainer } from "../components/styled";
+import TrendingNow from "../components/TrendingNow";
+import Upcoming from "../components/Upcoming";
+
+export default function MainPage() {
+  return (
+    <PageContainer>
+      <SearchBar />
+      <MovieList />
+      <TrendingNow />
+      <Upcoming></Upcoming>
+    </PageContainer>
+  );
+}
