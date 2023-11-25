da4be0a173be0579c648f8046f23ce419c299384
johanaller <93586332+johanaller@users.noreply.github.com>
2023-11-25 14:01:17 +0100

    Initial commit

---
+++ src/components/SimpleMovieCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+import movieApiClient from "../utils/movieApiClient";
+
+export default function SimpleMovieCard({ movieData }: { movieData: Movie }) {
+  const navigate = useNavigate();
+
+  const onCardClick = (event: any) => {
+    navigate(`/movie/${movieData.id}`);
+  };
+
+  return (
+    <SimpleMovieCardContainer>
+      <SimpleMovieCardImage
+        src={movieApiClient.buildMoviePosterUrl(movieData.poster_path)}
+        height="174"
+        onClick={onCardClick}
+        data-testid={`simple-movie-card-${movieData.id}`}
+      ></SimpleMovieCardImage>
+    </SimpleMovieCardContainer>
+  );
+}
+
+const SimpleMovieCardContainer = styled.div`
+  margin-left: 2px;
+  margin-right: 2px;
+`;
+
+const SimpleMovieCardImage = styled.img`
+  &:hover {
+    cursor: pointer;
+  }
+`;
